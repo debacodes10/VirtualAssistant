d41a8fcdd7ba9e9db140f16306e06235ab323be5
debacodes10 <debadyuti1010@gmail.com>
2023-04-20 19:57:09 +0530

    adding NLP model
    train and test NLP model and add to resources

---
+++ .venv/flaskapi.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+
+model = pickle.load(open('F:\\Test Software\\NLP 2.0\\.venv\\model.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def display():
+    return jsonify("Hello world!")
+
+@app.route('/intent', methods=['POST'])
+def predict():
+    text = request.form.get('text')
+    inp_q = np.array([text])
+    result = model.predict(inp_q)[0]
+    return jsonify({'intent': str(result)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, ssl_context=('certificate.pem','key.pem'))
